9ae9d10bdc80e32b1b0a6fb2991fc538c867935e
Ujjwal Kirti <ujjwal.k@dotproduct.in>
2023-12-18 19:54:27 +0530

    added some env file changes

--- src/db.ts
+++ src/db.ts
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
-import dotenv from "dotenv";
-
-dotenv.config()
+require("dotenv").config();
 
 
-function connectToDb(){
-    mongoose
-      .connect(process.env.MONGODB_URI?.toString())
-      .then(() => console.log("Connected!"))
-      .catch((err: any) => console.log(err));
+function connectToDb() {
+  mongoose
+    .connect(process.env.MONGODB_URI?.toString())
+    .then(() => console.log("Connected!"))
+    .catch((err: any) => console.log(err));
 }
 
 
-export {connectToDb}
+export { connectToDb }
