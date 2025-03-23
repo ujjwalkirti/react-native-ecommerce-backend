edda38320fe742fecba3a6ecb9f94dd0af78067b
Ujjwal Kirti <kirti.chargers@gmail.com>
2025-03-23 13:58:11 +0530

    update: verifyToken middleware to include security for tokens

--- src/middleware/jwt/index.ts
+++ src/middleware/jwt/index.ts
@@ -2,12 +2,21 @@
 
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import Token from "../../models/Token";
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization'];
     if (authHeader) {
         const token = authHeader.split(" ")[1];
-        jwt.verify(token, process.env.JWT_SEC as string, (err: any, user: any) => {
+        
+        //check if this token is there in db
+        const tokenExists = Token.findOne({ token });
+        if (!tokenExists) {
+            return res.status(403).json("Token is not valid!");
+        }
+        
+        //chek if the token belongs to this user only
+        const user = jwt.verify(token, process.env.JWT_SEC as string, (err: any, user: any) => {
             if (err) return res.status(403).json("Token is not valid!");
             next();
         });
