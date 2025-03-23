4aab87ff5f64b7a3b8551b4e4115eaeee0863b70
Ujjwal Kirti <kirti.chargers@gmail.com>
2025-03-23 18:00:05 +0530

    bugfix: token mismatch bug in middleware

--- src/middleware/jwt/index.ts
+++ src/middleware/jwt/index.ts
@@ -4,20 +4,23 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import Token from "../../models/Token";
 
-const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization'];
     if (authHeader) {
         const token = authHeader.split(" ")[1];
         
         //check if this token is there in db
-        const tokenExists = Token.findOne({ token });
+        const tokenExists = await Token.findOne({ token });
         if (!tokenExists) {
             return res.status(403).json("Token is not valid!");
         }
         
         //chek if the token belongs to this user only
-        const user = jwt.verify(token, process.env.JWT_SEC as string, (err: any, user: any) => {
+        jwt.verify(token, process.env.JWT_SEC as string, (err: any, user: any) => {
             if (err) return res.status(403).json("Token is not valid!");
+            if (user.email !== tokenExists.email) {
+                return res.status(403).json("Token is not valid!");
+            }
             next();
         });
     } else {
